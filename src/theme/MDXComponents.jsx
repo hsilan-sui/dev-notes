9d26302075359b75798a234ac6715895efbed514
hsilan-sui <suihsilan1101@icloud.com>
2025-12-23 23:54:52 +0800

    feat: LINE OA

---
+++ src/theme/MDXComponents.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import MDXComponents from "@theme-original/MDXComponents";
+import Highlight from "@site/src/components/ui/Highlight";
+
+export default {
+  ...MDXComponents,
+  Highlight,
+};
